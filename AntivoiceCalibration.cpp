9328a2b68410e27d7cc68abf01fe32635ac00978
Rishi Kapadia <r4kapadi@uwaterloo.ca>
2016-07-23 14:28:11 -0400

    Placed calibration setup into a header file.

--- AntivoiceCalibration.cpp
+++ AntivoiceCalibration.cpp
@@ -14,58 +14,9 @@
 	-find a way to perform obs signal analysis without storing the entire signal in memory (lower memory consumption)
 		-perhaps send it via the serial debug monitor
 */
-	
-#define WINDOWS 1
 
-#ifdef WINDOWS
-	#include "stdafx.h"
-#endif
+#include "CalibrationSetup.h"
 
-#include <math.h>
-#include <stdlib.h>
-#include "fir_filtering.h"
-
-#ifndef PI
-# define PI	3.14159265358979323846264338327950288
-#endif
-
-
-#define SPEED_OF_SOUND (343)
-#define TRANSDUCER_OBS_SEPARATION_DISTANCE_METERS (1.3)
-#define WHITE_NOISE_STD_DEVIATION (0.05)
-
-#define MIN(a,b) (((a)<(b))?(a):(b))
-#define MAX(a,b) (((a)>(b))?(a):(b))
-
-struct calibration {
-	double frequency; //frequency of tone
-	double amplitude; //amplitude of tone
-	double eqgain; //gain needed to equalize (scalar factor, not dB)
-	double rtt; //delay in seconds it took for round trip time
-	double internaltime; //rtt minus airtime (does not include computation time)
-};
-typedef struct calibration CALIB_t;
-
-struct amplitude_delay {
-	double amplitude; //amplitude of signal
-	double delay; //delay in seconds it took for round trip time
-};
-typedef struct amplitude_delay AMPDELAY_t;
-
-
-
-/* Global variables */
-double duration = 1; //seconds for a given test signal
-const unsigned int freq_count_pos = 4;
-double frequencies[freq_count_pos] = { 10, 50, 100, 200 };
-double system_gain[freq_count_pos] = { 0.9, 0.8, 0.7, 0.6 };
-double system_delay[freq_count_pos] = { 0.1,0.2,0.3,0.4 };
-
-double channel_delay_seconds = (double)TRANSDUCER_OBS_SEPARATION_DISTANCE_METERS / (double)SPEED_OF_SOUND;
-
-const unsigned int amplitude_count = 3;
-double amplitudes[amplitude_count] = { 1, 0.8, 0.6 };
-/* Global variables end */
 
 /*
 	Helper function for getting frequency index from Hertz
@@ -431,17 +382,14 @@ int main(int argc, char ** argv)
 {
 	//TODO: make frequencies be passed as an array by command line?
 
-	//samples per second or resolution of sinusoid (highest freq * some constant)
-	//unsigned int fs = (unsigned int)round(frequencies[freq_count_pos - 1] * 250);
-	unsigned int fs = 8000;
-
+	
 	//Calculate channel delay in terms of samples
 	unsigned int channel_delay_samples = (unsigned int)round(channel_delay_seconds * fs);
 	printf("\n[Warning] channel delay of %.6f seconds rounded to %.6f seconds due to nearest sample.",
 		channel_delay_seconds, channel_delay_samples / (double) fs);
 	channel_delay_seconds = channel_delay_samples / (double)fs;
 	
-	CALIB_t calib_results[freq_count_pos * amplitude_count]; //stores the calibratoin results
+	CALIB_t calib_results[freq_count_pos * amplitude_count]; //stores the calibration results
 
 	printf("\nFreq | Amplitude  | Sysgain | SysDelay| eqGain  |  RTT        | Int. Delay  | \n");
 
---
+++ CalibrationSetup.h
@@ -0,0 +1,52 @@
+#define WINDOWS 1
+
+#ifdef WINDOWS
+#include "stdafx.h"
+#endif
+
+#include <math.h>
+#include <stdlib.h>
+#include "fir_filtering.h"
+
+#ifndef PI
+# define PI	3.14159265358979323846264338327950288
+#endif
+
+#define SPEED_OF_SOUND (343)
+#define TRANSDUCER_OBS_SEPARATION_DISTANCE_METERS (1.3)
+#define WHITE_NOISE_STD_DEVIATION (0.05)
+
+#define MIN(a,b) (((a)<(b))?(a):(b))
+#define MAX(a,b) (((a)>(b))?(a):(b))
+
+struct calibration {
+	double frequency; //frequency of tone
+	double amplitude; //amplitude of tone
+	double eqgain; //gain needed to equalize (scalar factor, not dB)
+	double rtt; //delay in seconds it took for round trip time
+	double internaltime; //rtt minus airtime (does not include computation time)
+};
+typedef struct calibration CALIB_t;
+
+struct amplitude_delay {
+	double amplitude; //amplitude of signal
+	double delay; //delay in seconds it took for round trip time
+};
+typedef struct amplitude_delay AMPDELAY_t;
+
+
+
+/* Global variables */
+const unsigned int fs = 8000; //sinusoidal resolution, as well as sampling rate
+const double duration = 1; //seconds for a given test signal
+
+const unsigned int freq_count_pos = 4;
+double frequencies[freq_count_pos] = { 10, 50, 100, 200 };
+double system_gain[freq_count_pos] = { 0.9, 0.8, 0.7, 0.6 };
+double system_delay[freq_count_pos] = { 0.1,0.2,0.3,0.4 };
+
+double channel_delay_seconds = (double)TRANSDUCER_OBS_SEPARATION_DISTANCE_METERS / (double)SPEED_OF_SOUND;
+
+const unsigned int amplitude_count = 3;
+double amplitudes[amplitude_count] = { 1, 0.8, 0.6 };
+/* Global variables end */
